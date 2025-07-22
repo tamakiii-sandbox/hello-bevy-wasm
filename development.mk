b98447e40c3ab4daf8674d600225e408678942f2
tamakiii <tamakiii@users.noreply.github.com>
2025-07-22 17:00:49 +0900

    .cargo

--- development.mk
+++ development.mk
@@ -4,9 +4,9 @@ help:
 	@cat $(firstword $(MAKEFILE_LIST))
 
 setup:
-	cargo install wasm-pack
-	cargo install basic-http-server
-	cargo install cargo-watch
+	cargo install --root .cargo wasm-pack
+	cargo install --root .cargo basic-http-server
+	cargo install --root .cargo cargo-watch
 
 teardown:
 	# uninstall wasm-pack
@@ -14,9 +14,9 @@ teardown:
 	# uninstall cargo-watch
 
 serve:
-	basic-http-server .
+	.cargo/bin/basic-http-server .
 
 serve-watch:
 	@echo "Starting development server with hot reload..."
 	@echo "The server will rebuild WASM when Rust source files change"
-	cargo watch -i pkg/ -x 'build --target wasm32-unknown-unknown' -s 'make -C . -f wasm.mk build' -s 'basic-http-server .' --no-restart
+	.cargo/bin/cargo-watch -i pkg/ -x 'build --target wasm32-unknown-unknown' -s 'make -C . -f wasm.mk build' -s '.cargo/bin/basic-http-server .' --no-restart
