8f9fed09fe2daf65bef5d9f23dc54e5172ce4883
tamakiii <tamakiii@users.noreply.github.com>
2025-07-22 16:58:09 +0900

    serve-dev

--- development.mk
+++ development.mk
@@ -1,4 +1,4 @@
-.PHONY: help setup teardown serve
+.PHONY: help setup teardown serve serve-dev
 
 help:
 	@cat $(firstword $(MAKEFILE_LIST))
@@ -6,10 +6,17 @@ help:
 setup:
 	cargo install wasm-pack
 	cargo install basic-http-server
+	cargo install cargo-watch
 
 teardown:
 	# uninstall wasm-pack
 	# uninstall basic-http-server
+	# uninstall cargo-watch
 
 serve:
 	basic-http-server .
+
+serve-dev:
+	@echo "Starting development server with hot reload..."
+	@echo "The server will rebuild WASM when Rust source files change"
+	cargo watch -i pkg/ -x 'build --target wasm32-unknown-unknown' -s 'make -C . -f wasm.mk build' -s 'basic-http-server .' --no-restart
